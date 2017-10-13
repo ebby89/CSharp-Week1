a358139215079388a7eea80b54eece85ddfb7520
Kyle Ebersole <kyle.m.ebersole@gmail.com>
2017-10-13 15:03:55 -0700

    Break everything. What little there is. Create contact class. Create controller that doesn't work other than homepage. Add links to cshtml that don't work at all.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,10 +7,28 @@ namespace AddressBookProject.Controllers
 {
     public class HomeController : Controller
     {
-        [HttpGet("/")]
+        [HttpGet("/"), ActionName("ContactList")]
         public ActionResult Index()
         {
-            return View();
+            return View(Contact.GetAll());
         }
+
+        [HttpPost("/contacts/new"), ActionName("ContactForm")]
+        public ActionResult NewContact()
+        {
+          string contactName = Request.Form["new-contact-name"];
+          string contactPhone = Request.Form["new-contact-phone"];
+          string contactAddress = Request.Form["new-contact-address"];
+          Contact newContact = new Contact(contactName, contactPhone, contactAddress);
+          return View(Contact.GetAll());
+        }
+
+        // [HttpPost("/contacts/{id}"), ActionName("ContactInfo")]
+        // public ActionResult ContactInfo(int id)
+        // {
+        //   Contact selectedContact = Contact.Find(id);
+        //   return View(selectedPlace);
+        // }
+
     }
 }
--- Models/Contact.cs
+++ Models/Contact.cs
@@ -5,6 +5,62 @@ namespace AddressBookProject.Models
 {
   public class Contact
   {
-    
+    private string _name;
+    private string _phone;
+    private string _address;
+    private int _id;
+    public static List<Contact> _instances = new List<Contact>(){};
+
+    public Contact (string name, string phone, string address)
+    {
+      _name = name;
+      _phone = phone;
+      _address = address;
+      _instances.Add(this);
+      _id = _instances.Count;
+    }
+
+    public string GetName()
+    {
+      return _name;
+    }
+    public void SetName(string newName)
+    {
+      _name = newName;
+    }
+
+    public string GetPhone()
+    {
+      return _phone;
+    }
+    public void SetPhone(string newPhone)
+    {
+      _phone = newPhone;
+    }
+
+    public string GetAddress()
+    {
+      return _address;
+    }
+    public void SetAddress(string newAddress)
+    {
+      _address = newAddress;
+    }
+
+    public int GetId()
+    {
+      return _id;
+    }
+
+    public static List<Contact> GetAll()
+    {
+      return _instances;
+    }
+
+    public static Contact Find(int id)
+    {
+      return _instances[id];
+    }
+            //TODO -1 after id? 
   }
 }
